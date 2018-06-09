032f7fcb312355411732ca95ae9f74e55701a108
guypierce <14934009+guypierce@users.noreply.github.com>
2018-06-08 22:10:00 -0600

    added rankine

--- converter.js
+++ converter.js
@@ -1,6 +1,8 @@
+// Tempretures are converted from celsius
 const kelvin = 283; //kelvin tempreture for today
 const celsius = kelvin - 273; // celcius is 273 degrees less than kelvin
 const newton = celsius * (33/100);
+const rankine = celsius + 273.15 * (9/5);
 const fahrenheit = celsius * (9/5) + 32; // celcius to fahrenheit equation
 console.log(`The temperature is ${Math.floor(fahrenheit)} degrees Fahrenheit.`);
 console.log(`The temperature is ${Math.floor(newton)} degrees Newton.`);
