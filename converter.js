fde913371dc63d26e4c550542882c427f1c1ebb6
guypierce <14934009+guypierce@users.noreply.github.com>
2018-06-08 22:23:47 -0600

    added delisle

--- converter.js
+++ converter.js
@@ -1,8 +1,12 @@
 // Tempretures are converted from celsius
-const kelvin = 283; //kelvin tempreture for today
-const celsius = kelvin - 273; // celcius is 273 degrees less than kelvin
+// (TODO) Add user input functionality for custom tempreture conversion
+const kelvin = 283;
+const celsius = kelvin - 273;
 const newton = celsius * (33/100);
 const rankine = celsius + 273.15 * (9/5);
+const delisle = 100 - celsius * (3/2);
 const fahrenheit = celsius * (9/5) + 32; // celcius to fahrenheit equation
 console.log(`The temperature is ${Math.floor(fahrenheit)} degrees Fahrenheit.`);
 console.log(`The temperature is ${Math.floor(newton)} degrees Newton.`);
+console.log(`The temperature is ${Math.floor(rankine)} degrees rankine.`);
+console.log(`The temperature is ${Math.floor(delisle)} degrees delisle.`);
